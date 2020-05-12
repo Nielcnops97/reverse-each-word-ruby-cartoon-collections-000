e4fee261c32df7c73bbe7a63e9d3408af9a97031
Niel Cnops <nielcnops@outlook.com>
2020-05-12 20:14:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,5 @@ def reverse_each_word(sentence)
   sentence.collect do |word|  #colect returns the value of your method. do this with word next
     word.reverse  #reverses the word(s) in the original array
 	end
-  .join(" ")  #join coverts array into String 
+  .join(" ")  #join adds an argument which can be put between the array element when converted into a string.
 end
